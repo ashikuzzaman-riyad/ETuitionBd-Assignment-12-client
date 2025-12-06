a2dede4e3452be0029f5c7c1555a0dbdaf7dd105
riyadhossen74 <riyadhossen696969@gmail.com>
2025-12-06 20:43:23 +0600

    added folder structure

---
+++ src/components/cards/TuitionCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TuitionCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TuitionCard;
---
+++ src/components/dashboard/admin/TuitionManagement.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TuitionManagement = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TuitionManagement;
